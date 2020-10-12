ea22e9e302c8c14072d1a419ed88b84421cd30a4
boonyarit <boonyaritza93@hotmail.com>
2020-10-12 19:26:54 +0700

    update All files in terms of work process

--- ImgSlider-ByDennis/script.js
+++ ImgSlider-ByDennis/script.js
@@ -3,12 +3,14 @@ let activeImages = document.getElementsByClassName('active');
 
 for(var i=0; i < thumbnails.length; i++){
     thumbnails[i].addEventListener('mouseover', function() {
-        console.log(activeImages);
+        // console.log(activeImages);
         if(activeImages.length > 0) {
             activeImages[0].classList.remove('active')
         }
 
-        this.classList.add('active')
+        // add a Active classlist for making a clear image in class Thumbnail
+        this.classList.add('active');
+        // edit a Featured's src to be The image that was hovered (this.src = thumnails[i].src)
         document.getElementById('featured').src = this.src
     })
 }
@@ -17,9 +19,10 @@ let buttonRight = document.getElementById('slideRight');
 let buttonLeft = document.getElementById('slideLeft');
 
 buttonLeft.addEventListener("click", function() {
-    document.getElementById('slider').scrollLeft += 180
+    document.getElementById('slider').scrollLeft -= 180
+
 })
 
 buttonRight.addEventListener("click", function() {
-    document.getElementById('slider').scrollLeft -= 180
+    document.getElementById('slider').scrollLeft += 180
 })
